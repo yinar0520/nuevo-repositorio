30c33c93faa0171dfca41f776b0664b6b4fe0ac8
edwarbxp <edwarbxp@gmail.com>
2017-04-06 20:50:02 -0500

    Esta es una nueva clase que resta dos numeros

---
+++ Resta.java
@@ -0,0 +1,12 @@
+package suma;
+
+public class Resta {
+
+    public static void main(String[] args) {
+        int a=1;
+        int b=3;
+ 
+        System.out.println("LA RESTA ES "+(b-a));
+    }
+    
+}
